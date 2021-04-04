8e39a232a4ad8b1d8939f10575c7cef65986f6a8
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2021-04-04 22:54:09 +0530

    Pattern 5

---
+++ cpp_basic/pattern5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int count = 0, i, j;
+     
+     for(i = 0;i<4;i++){
+          for(j = 0;j<4;j++){
+               if(j+i < 3)
+                    cout<<" "<<" ";  
+               else
+                    cout<<"*"<<" ";                     
+          }
+          cout<<endl;
+     }
+
+}
