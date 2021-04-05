44e0a0cc37ddcf757239151f4866b34e9022c84c
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2021-04-05 13:40:15 +0530

    Matrix Subtraction

---
+++ cpp_basic/MATSUB.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     int i, j;
+     int a[2][3] = {1,2,3,4,5,6} , b[2][3] = {{7,8,9},{10,11,12}}, c[2][3];
+     
+     for(i = 0;i<2;i++){
+          for(j = 0;j<3;j++){
+               c[i][j] = a[i][j] - b[i][j];
+          }
+     }
+     
+     for(i = 0;i<2;i++){
+          for(j = 0;j<3;j++){
+               cout<<c[i][j]<<" ";
+          }
+          cout<<endl;
+     }
+
+}
