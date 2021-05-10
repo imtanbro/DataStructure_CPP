4ce93bbcbf39640bbd60d0f03d4b96a5f729f62f
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2021-05-10 11:48:44 +0530

    Cyclic Array

--- Array/CYCLICALARR.cpp
+++ Array/CYCLICALARR.cpp
@@ -4,6 +4,24 @@ using namespace std;
 int main(){
 
      int a[] = {1,2,3,4,5};
-     // {5,1,2,3,4}
+     //        {5,1,2,3,4}
+     //        {0,1,2,3,4}
+     int *p = a;
+     int temp,end = 4;
+     temp = a[end];
+     for(int i =4; i>=0 ;i--){
+          if(i != 0)
+               a[i] = a[i - 1];
+          else
+               a[i] = temp;
+          // cout<<a[i]<<" ";   
+     }
+
+     for (auto x:a){
+          cout<<x<<" ";
+     }
+
+     cout<<endl;
+     
          
 }
