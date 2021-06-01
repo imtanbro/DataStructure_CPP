b1787a9e08a33c0def06d61f8586611bf38d8891
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2021-06-01 16:55:32 +0530

    Type conversion

---
+++ Array/expinitandnoninit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // float a = 43/3;
+    // cout<<a;
+
+    int a = 3,c = 4;
+    float b = 5.8;
+
+    c = a + b + c;
+
+    cout<<c<<endl;
+
+    // int a[5] = {};
+    // for(auto i:a)
+    //     cout<<i<<" ";
+    // cout<<endl;
+
+}
+
