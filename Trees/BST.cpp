a8358158f152c586bad2eedf51b8babe36e39cc1
Tanmay Gangurde <tanmay.gangurde@gmail.com>
2021-11-20 10:39:45 +0530

    Binary Search Tree using LinkedList

---
+++ Trees/BST.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Tree
+{
+    node *root;
+
+public:
+    // void insertElement(int data){
+    //     node *temp = new node(data);
+    //     if(root == NULL){
+    //         root = temp;
+    //     }else if(root -> data > data){
+    //         root -> left = temp;
+    //     }else{
+    //         root -> right = temp;
+    //     }
+    // }
+
+    // This wont work as we need to use recursive function in order to ad element till the end;
+
+    node *insertElement(int data)
+    {
+        // node *temp = new node(data);
+        if (root == NULL)
+        {
+            return new node(data);
+        }
+        else if (root->data > data)
+        {
+            root->left = insertElement(data);
+        }
+        else if(root->data == data) return root;
+        else
+        {
+            root->right = insertElement(data);
+        }
+    }
+};
+
+int main()
+{
+}
